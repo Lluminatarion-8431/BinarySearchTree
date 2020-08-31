d1dc5e7de5b2890900c698c9789737489b49022e
Lluminatarion-8431 <Captain.a.8431@gmail.com>
2020-08-31 08:54:32 -0700

    Init. BinarySearchTree project.

---
+++ BinarySearchTree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeProj
+{
+    class BinarySearchTree
+    {
+        public Node RootNode;
+        //public Node LastNode;
+
+        public void Add(Node nodeToAdd)
+        {
+            if (RootNode == null)
+            {
+                RootNode = nodeToAdd;
+                //LastNode = RootNode;
+            }
+            else
+            {
+                //LastNode.NextNode = nodeToAdd;
+                //LastNode = LastNode.NextNode;
+
+                Node currentNode = RootNode;
+                while (true)
+                {
+                    if (currentNode.NextNode == null)
+                    {
+                        currentNode.NextNode = nodeToAdd;
+                        break;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.NextNode;
+                    }
+                }
+
+
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree myList = new BinarySearchTree();
+
+            Node node1 = new Node(1);
+            Node node2 = new Node(2);
+            Node node3 = new Node(3);
+            Node node4 = new Node(4);
+            Node node5 = new Node(5);
+
+            myList.Add(node1);
+            myList.Add(node2);
+            myList.Add(node3);
+            myList.Add(node4);
+            myList.Add(node5);
+        }
+    }
+}
